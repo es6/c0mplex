120ec946ddbec2921bd9c6726fad02d613304797
es6 <benlin2003@gmail.com>
2023-08-17 04:45:02 -0400

    Completed setting up hook functions and our dllmain Setup function

--- csgo/csgo/src/dllmain.cpp
+++ csgo/csgo/src/dllmain.cpp
@@ -3,9 +3,49 @@
 #include <thread>
 #include <cstdint>
 
-//
-void Setup(const HMODULE instance) {
+#include "hooks.h"
 
+/*
+* Breakdown of Execution
+* 
+* We first inject our DLL (calls DllMain), then check if the reason is DLL_PROCESS_ATTACH
+* We then disable thread library calls because we aren't interested in thread notifications
+* 
+* Then we create a thread which runs Setup(const HMODULE instance) and if thread creation is successful
+* We then close our handle
+* 
+* 
+* If everything is fine, Setup(...) will be run in a separate thread which the function will try to setup gui
+*	- this attempts to create a temp window, getting the device, and saving the device.
+* 
+* We use this device created from our gui::Setup to then hook the 2 virtual functions: EndScene and Reset
+*	- if everything works, we sit in the while loop until we close our menu via pressing DELETE
+*	- if DELETE is pressed, we go to UNLOAD which destroys our hooks and gui then frees the library and exits the thread
+*/
+
+
+void Setup(const HMODULE instance) {
+	try {
+		// Initialize our cheat
+		gui::Setup();
+		hooks::Setup(); 
+	}
+	catch (const std::exception& error) {
+		MessageBeep(MB_ICONERROR);
+		MessageBox(0, error.what(), "c0mplex - ERROR", MB_OK | MB_ICONEXCLAMATION);
+
+		goto UNLOAD;
+	} 
+
+	while (!GetAsyncKeyState(VK_DELETE)) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	}
+
+UNLOAD:
+	hooks::Destroy();
+	gui::Destroy();
+
+	FreeLibraryAndExitThread(instance, 0);
 }
 
 BOOL WINAPI DllMain(const HMODULE instance, const std::uintptr_t reason, const void* reserved) {
